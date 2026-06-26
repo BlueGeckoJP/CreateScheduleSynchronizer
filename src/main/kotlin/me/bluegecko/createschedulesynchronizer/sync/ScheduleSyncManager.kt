adc1264a32632d63b27616df4a35618afd5a1507
BlueGeckoJP <kei09doya@gmail.com>
2026-06-26 10:19:26 +0900

    feat: add methods for saving schedules from player hands to storage

--- src/main/kotlin/me/bluegecko/createschedulesynchronizer/sync/ScheduleSyncManager.kt
+++ src/main/kotlin/me/bluegecko/createschedulesynchronizer/sync/ScheduleSyncManager.kt
@@ -1,8 +1,11 @@
 package me.bluegecko.createschedulesynchronizer.sync
 
 import com.simibubi.create.AllDataComponents
+import com.simibubi.create.content.trains.schedule.Schedule
 import me.bluegecko.createschedulesynchronizer.item.SynchronizedScheduleItem
 import net.minecraft.server.level.ServerLevel
+import net.minecraft.server.level.ServerPlayer
+import net.minecraft.world.InteractionHand
 import net.minecraft.world.item.ItemStack
 
 object ScheduleSyncManager {
@@ -35,5 +38,42 @@ object ScheduleSyncManager {
         }
 
         return false
+    }
+
+    fun saveMainHandScheduleToStore(player: ServerPlayer): SaveResult {
+        return saveHeldScheduleToStore(player, InteractionHand.MAIN_HAND)
+    }
+
+    fun saveHeldScheduleToStore(
+        player: ServerPlayer,
+        hand: InteractionHand
+    ): SaveResult {
+        return saveItemToStore(
+            stack = player.getItemInHand(hand),
+            level = player.serverLevel()
+        )
+    }
+
+    fun saveItemToStore(
+        stack: ItemStack,
+        level: ServerLevel
+    ): SaveResult {
+        if (stack.item !is SynchronizedScheduleItem) {
+            return SaveResult.NOT_SYNCHRONIZED_SCHEDULE
+        }
+
+        val syncId = SynchronizedScheduleItem.getSyncId(stack) ?: return SaveResult.NOT_LINKED
+
+        val scheduleTag = stack.get(AllDataComponents.TRAIN_SCHEDULE) ?: Schedule().write(level.registryAccess())
+
+        ScheduleSyncSavedData.get(level).putScheduleTag(syncId, scheduleTag.copy())
+
+        return SaveResult.SAVED
+    }
+
+    enum class SaveResult {
+        NOT_SYNCHRONIZED_SCHEDULE,
+        NOT_LINKED,
+        SAVED
     }
 }
